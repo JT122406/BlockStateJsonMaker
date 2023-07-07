3cffab01b38f7a5cf10f00b6269c98ce2ab73f40
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-06 22:09:45 -0400

    Fix Kotlin File being uploaded as default file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    // Prompt for the mod ID
+    println("Enter the mod ID:")
+    val modId = readLine() ?: return
+
+    // Prompt for the folder path
+    println("Enter the folder path:")
+    val folderPath = readLine() ?: return
+
+    // Create a File object for the folder
+    val folder = File(folderPath)
+
+    // Create a new folder for the JSON files
+    val jsonFolder = File(folder, "json")
+    if (!jsonFolder.exists()) jsonFolder.mkdir()
+    
+
+    // Get the list of PNG files in the folder
+    val pngFiles = folder.listFiles { _, name -> name.endsWith(".png") }
+
+    if (pngFiles != null)
+        for (pngFile in pngFiles) {
+            val fileName = pngFile.nameWithoutExtension
+            val jsonFile = File(jsonFolder, "$fileName.json")
+
+            // Create the JSON content
+            val jsonContent = """
+                {
+                  "variants": {
+                    "": {
+                      "model": "$modId:block/$fileName"
+                    }
+                  }
+                }
+            """.trimIndent()
+
+            // Write the JSON content to the file
+            jsonFile.writeText(jsonContent)
+
+            println("Created JSON file: ${jsonFile.absolutePath}")
+        }
+    else println("No PNG files found in the folder.")
 }
